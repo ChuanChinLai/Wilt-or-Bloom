d5f7b630005ebd71177fd2bee7c33788f28fc0cb
Chuan-Chin Danny Lai <chuanchinlai@gmail.com>
2016-11-20 00:03:20 -0700

    updated BGM

---
+++ Assets/Scripts/BGMManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGMManager : MonoBehaviour 
+{
+
+
+    public AudioClip _AudioClip_day;
+    public AudioClip _AudioClip_night;
+
+    AudioSource _AudioSource;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        _AudioSource = GetComponent<AudioSource>();
+        _AudioSource.clip = _AudioClip_day;
+        _AudioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+
+            if (_AudioSource.clip == _AudioClip_day)
+            {
+                _AudioSource.clip = _AudioClip_night;
+                _AudioSource.Play();
+            }
+            else
+            {
+                _AudioSource.clip = _AudioClip_day;
+                _AudioSource.Play();
+            }
+        }
+	}
+}
